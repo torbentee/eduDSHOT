6031ebe2aefffd6844ff111d2eb9c6c50aada7e5
Niklas Schünemann <niklas.schuenemann@stud.hs-hannover.de>
2018-04-25 22:40:39 +0200

    fixed django.sh crudini

--- scripts/django.sh
+++ scripts/django.sh
@@ -4,11 +4,11 @@ sudo apt install -y python python-pip mysql-client libmysqlclient-dev crudini
 cd /home/ubuntu
 git clone https://github.com/torbentee/eduDSExample.git
 cd eduDSExample
-crudini --set secret.ini DB SECRET_KEY $db_password
-crudini --set secret.ini DB DBHOST $db_ipaddress
-crudini --set secret.ini DB DBNAME $db_name
-crudini --set secret.ini DB DBUSER $db_user
-crudini --set secret.ini DB DBPW $db_password
+crudini --set secrets.ini DB SECRET_KEY $db_password
+crudini --set secrets.ini DB DBHOST $db_ipaddress
+crudini --set secrets.ini DB DBNAME $db_name
+crudini --set secrets.ini DB DBUSER $db_user
+crudini --set secrets.ini DB DBPW $db_password
 
 sudo pip install -r requirements.txt
 # todo ändern der sercret.txt um datenbank verbindung per script einzustellen
